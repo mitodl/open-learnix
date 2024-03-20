882a2e8e34bdae25f883255ebda07ef240b57f7f
Nathan Levesque <nlevesq@mit.edu>
2024-03-20 11:50:42 -0400

    Added support for a top-level keycloak

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
-  description = "Open Learing engineering nix toolset.";
+  description = "Open Learning engineering nix toolset.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell.url = "github:numtide/devshell";
-
-    # this is used solely for this flake repo
-    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
+    devenv.url = "github:cachix/devenv/caf1a2ea564c31f2ee70bc67af56f77a2758848a";
   };
 
   outputs = inputs @ {
@@ -22,8 +19,7 @@
       systems = import systems;
 
       imports = [
-        inputs.devshell.flakeModule
-        inputs.pre-commit-hooks-nix.flakeModule
+        inputs.devenv.flakeModule
       ];
 
       perSystem = {
@@ -34,49 +30,33 @@
         pkgs = import inputs.nixpkgs {inherit system;};
         unstable = import inputs.nixpkgs-unstable {inherit system;};
       in {
-        devshells.default = {
-          devshell = {
-            packages = [
-              pkgs.git
-              pkgs.pre-commit
-              pkgs.nodejs
-              pkgs.python3
-              pkgs.poetry
-              pkgs.gcc
-              # newer versions of ruff require a newer version of rustc/cargo to build
-              # these are only available in unstable at this time
-              unstable.cargo
-              unstable.rustc
-            ];
+        devenv.shells.default = {
+          packages = [
+            pkgs.git
+            pkgs.pre-commit
+            pkgs.nodejs
+            pkgs.python3
+            pkgs.poetry
+            pkgs.gcc
+            # newer versions of ruff require a newer version of rustc/cargo to build
+            # these are only available in unstable at this time
+            unstable.cargo
+            unstable.rustc
+          ];
 
-            startup.pre-commit.text = ''
-              ${config.pre-commit.installationScript}
-            '';
+          env = {
+            PRE_COMMIT_HOME = "$DEVENV_ROOT/.cache/pre-commit";
+            PIP_NO_BINARY = "ruff";
           };
 
-          env = [
-            {
-              name = "PRE_COMMIT_HOME";
-              eval = "$PRJ_ROOT/.cache/pre-commit";
-            }
-            {
-              name = "PIP_NO_BINARY";
-              value = "ruff";
-            }
-          ];
-        };
-
-        formatter = pkgs.alejandra;
-
-        pre-commit = {
-          inherit pkgs;
-          settings = {
+          pre-commit = {
             hooks = {
-              actionlint.enable = true;
+              # actionlint.enable = true;
               alejandra.enable = true;
-              markdownlint.enable = true;
-              yamllint.enable = true;
+              # markdownlint.enable = true;
+              # yamllint.enable = true;
             };
+
             settings = {
               markdownlint.config = {
                 # party like it's not 1999
@@ -88,7 +68,15 @@
               };
             };
           };
+
+          scripts = {
+            ol-dc.exec = ''
+              COMPOSE_FILE=$DEVENV_ROOT/docker-compose.yml docker compose $@
+            '';
+          };
         };
+
+        formatter = pkgs.alejandra;
       };
     };
 }
