b61937ab1adc18450d995bf1f22751eef6f14a4c
Nathan Levesque <nlevesq@mit.edu>
2026-01-14 12:36:22 -0500

    Fix workspaces

--- devshells/release-script.nix
+++ devshells/release-script.nix
@@ -5,13 +5,9 @@
     ...
   }: {
     devenv.shells.release-script = {
-      env = {
-        UV_PROJECT_ENVIRONMENT = lib.mkForce "./repos/release-script/.venv/";
-      };
-
       languages.python = {
         enable = true;
-        package = pkgs.python314;
+        package = pkgs.python313;
         uv = {
           enable = true;
           package = pkgs.uv;
